f96cf3aee80aac91355c2784a60f9a8480015631
Facundo Wade <wadefacundo@gmail.com>
2021-06-18 02:15:32 -0300

    inclusion maven, spring, falta hibernate

---
+++ src/frgp/utn/edu/ar/controller/LoginController.java
@@ -0,0 +1,26 @@
+package frgp.utn.edu.ar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping("Login.html")
+	public ModelAndView Login(){
+		ModelAndView MV = new ModelAndView();
+		MV.setViewName("Login"); 
+		return MV;
+	}
+	
+	 @RequestMapping("Dashboard.html")
+	public ModelAndView Ingresar(String txtUsername, String txtPassword)
+	{
+		ModelAndView MV = new ModelAndView();
+		MV.addObject("Usuario", txtUsername);
+		MV.addObject("Password", txtPassword);
+		MV.setViewName("Dashboard");
+		return MV;
+	}
+}
