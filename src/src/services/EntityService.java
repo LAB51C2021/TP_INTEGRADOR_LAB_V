6845d0ad12aa9c2b9a74b174bbb3002c1b7e02cb
Gabriel A. Robledo <gaby-2011@hotmail.com.ar>
2021-07-13 00:19:29 -0300

    Agregué unos comentarios.

--- src/src/services/EntityService.java
+++ src/src/services/EntityService.java
@@ -22,41 +22,49 @@ public class EntityService<T> implements IEntityService<T>
 	@Autowired
 	private HibernateConnector hibernateConnector;
 	
+	// Devuelve una entidad de la base de datos con la ID que se pase por parametro.
 	@Override
 	public T FirstOrDefault(int id) 
 	{
 		return (T) hibernateConnector.GetEntityKey(id, entity.getClass().getName());
 	}
 
+	
+	// Devuelve una entidad de la base de datos con el where que se pasa por parametro.
 	@Override
 	public T FirstOrDefault(String where) 
 	{
 		return (T) hibernateConnector.GetFirstOrDefaultWhere(entity.getClass().getName(), where);
 	}
 
+	// Devuelve una lista con todos los registros.
 	@Override
 	public List<T> GetAll() 
 	{
 		return (List<T>) hibernateConnector.GetList(entity.getClass().getName());
 	}
-	
+
+	// Devuelve una lista con todos los registros que cumplan con where que se pasa por parametro.
 	@Override
 	public List<T> GetAll(String where) {
 		return (List<T>) hibernateConnector.GetList(Cuenta.class.getSimpleName(), where);
 	}
 
+	// Agrega una nueva entidad.
 	@Override
 	public void Add(T entity) 
 	{
 		hibernateConnector.AddEntity(entity);
 	}
 
+	// Modificar una entidad, siempre enviar la instancia que se obtenga en el FirstOrDefault.
 	@Override
 	public void Update(T entity) 
 	{
 		hibernateConnector.UpdateEntity(entity);
 	}
 
+	// Elimina una entidad, siempre enviar la instancia que se obtenga en el FirstOrDefault.
 	@Override
 	public void Delete(T entity) 
 	{
--- src/src/services/IEntityService.java
+++ src/src/services/IEntityService.java
@@ -4,17 +4,24 @@ import java.util.List;
 
 public interface IEntityService<T>
 {
+	// Devuelve una entidad de la base de datos con la ID que se pase por parametro.
 	T FirstOrDefault(int id);
-	
+
+	// Devuelve una entidad de la base de datos con el where que se pasa por parametro.
 	T FirstOrDefault(String where);
-	
+
+	// Devuelve una lista con todos los registros.
 	List<T> GetAll();
 
+	// Devuelve una lista con todos los registros que cumplan con where que se pasa por parametro.
 	List<T> GetAll(String where);
-	
+
+	// Agrega una nueva entidad.
 	void Add(T entity);
-	
+
+	// Modificar una entidad, siempre enviar la instancia que se obtenga en el FirstOrDefault.
 	void Update(T entity);
-	
+
+	// Elimina una entidad, siempre enviar la instancia que se obtenga en el FirstOrDefault.
 	void Delete(T entity);
 }
